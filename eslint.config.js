6cabe105778e04a959b4f9f7bd44d2d377f76c7f
tamiroh <tamiroh71@gmail.com>
2026-09-18 20:19:43 +0900

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+    globalIgnores(["dist/"]),
+    {
+        files: ["**/*.{ts,tsx}"],
+        extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs["recommended-latest"]],
+    },
+]);
